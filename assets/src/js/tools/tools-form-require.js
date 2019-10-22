bde3fcd8bd66729ded9370fd6da084bfabb0fe8c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-22 02:26:06 +0200

    Refactoring code. Idented js and changed name functions.

--- assets/src/js/tools/tools-form-require.js
+++ assets/src/js/tools/tools-form-require.js
@@ -6,16 +6,14 @@
 
 
 
-function formFieldRequired()
-{
-    let required = "<span class='c-required'>*</span>";
+function formFieldRequired() {
+	let required = "<span class='c-required'>*</span>";
 
 	// form field
-    $(".c-form__field[required]").closest(".c-form__item").find(".c-label").append(required);
+	$(".c-form__field[required]").closest(".c-form__item").find(".c-label").append(required);
 
 	// check list
 	$(".c-check__input[required]").closest(".c-check").find(".c-check__label").append(required);
 	$(".c-form__list .c-check__input[required]").closest(".c-form__item").find(".c-label").append(required);
 	$(".c-form__list .c-check__label").find(".c-required").remove();
-
 }
--- assets/src/js/tools/tools-form-validation-ckeditor.js
+++ assets/src/js/tools/tools-form-validation-ckeditor.js
@@ -9,75 +9,71 @@
 // FORM - CKEDITOR
 // =================================================
 
-function ckeditor_validate_findField()
-{
-    var formId   = $("form").attr("id");
-    var textarea = $("#" + formId + " " + ".ckeditor");
-    var arrayTextareaId = [];
-    textarea.each(function ()
-    {
-        arrayTextareaId.push($(this).attr("id"));
-    });
-    return arrayTextareaId;
+function ckeditorValidateFindField() {
+	var formId = $("form").attr("id");
+	var textarea = $("#" + formId + " " + ".ckeditor");
+	var arrayTextareaId = [];
+	textarea.each(function () {
+		arrayTextareaId.push($(this).attr("id"));
+	});
+	return arrayTextareaId;
 }
 
 
-function ckeditor_validate(textareaId)
-{
-    var ckeditorIframe = $("#cke_" + textareaId + " iframe"),
-        ckeditorText   = ckeditorIframe
-            .contents()
-            .find("body")
-            .html(),
-        ckeditorText   = ckeditorText.replace(/<[^>]+>/g, ""),
-        ckeditorDiv    = ckeditorIframe
-            .parent()
-            .parent()
-            .parent(),
-        textarea       = ckeditorDiv.parent().find("textarea"),
-        textareaLabel  = "label[for='" + textareaId + "'] span";
-    
-    var errorLabel   =
-            "<label id='" +
-            textareaId +
-            "-error' class='error' for='" +
-            textareaId +
-            "'>" +
-            field_message_required +
-            "</label>",
-        errorLabelId = textareaId + "-error";
-    
-    
+function ckeditorValidate(textareaId) {
+	var ckeditorIframe = $("#cke_" + textareaId + " iframe"),
+		ckeditorText = ckeditorIframe
+			.contents()
+			.find("body")
+			.html(),
+		ckeditorText = ckeditorText.replace(/<[^>]+>/g, ""),
+		ckeditorDiv = ckeditorIframe
+			.parent()
+			.parent()
+			.parent(),
+		textarea = ckeditorDiv.parent().find("textarea"),
+		textareaLabel = "label[for='" + textareaId + "'] span";
 
-    function knowEvent(e)
-    {
-        if (!e) {
-            var e = window.event;
-        }
-        if (e.type == "submit" && ckeditorText == "") {
-            e.preventDefault();
-        }
-    }
-    
-    if (
-        $("#" + textareaId)
-            .parent()
-            .find(textareaLabel)
-            .hasClass("c-required")
-    ) {
-        if (ckeditorText == "") {
-            if (!$("#" + errorLabelId).length) {
-                textarea.parent().append(errorLabel);
-            }
-            ckeditorDiv.addClass("error");
-            
-            document.onsubmit   = knowEvent;
-            document.onblur     = knowEvent;
-            document.onkeypress = knowEvent;
-            document.onkeypress = knowEvent;
-        } else {
-            $("#" + errorLabelId).remove();
-            ckeditorDiv.removeClass("error");
-        }
-    }
+	var errorLabel =
+		"<label id='" +
+		textareaId +
+		"-error' class='error' for='" +
+		textareaId +
+		"'>" +
+		field_message_required +
+		"</label>",
+		errorLabelId = textareaId + "-error";
+
+
+
+	function knowEvent(e) {
+		if (!e) {
+			var e = window.event;
+		}
+		if (e.type == "submit" && ckeditorText == "") {
+			e.preventDefault();
+		}
+	}
+
+	if (
+		$("#" + textareaId)
+			.parent()
+			.find(textareaLabel)
+			.hasClass("c-required")
+	) {
+		if (ckeditorText == "") {
+			if (!$("#" + errorLabelId).length) {
+				textarea.parent().append(errorLabel);
+			}
+			ckeditorDiv.addClass("error");
+
+			document.onsubmit = knowEvent;
+			document.onblur = knowEvent;
+			document.onkeypress = knowEvent;
+			document.onkeypress = knowEvent;
+		} else {
+			$("#" + errorLabelId).remove();
+			ckeditorDiv.removeClass("error");
+		}
+	}
 }
