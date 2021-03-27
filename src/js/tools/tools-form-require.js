b773fb314c181a1934d68e15f59ffb5244076da4
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-03-28 00:00:46 +0100

    Removed prefixes from the class names
    - Removed prefix 'c-' of component
    - Removed prefix 't-' of tool

--- src/js/tools/tools-form-require.js
+++ src/js/tools/tools-form-require.js
@@ -7,13 +7,13 @@
 
 
 function formFieldRequired() {
-	let required = "<span class='c-required'>*</span>";
+	let required = "<span class='required'>*</span>";
 
 	// form field
-	$(".c-form__field[required]").closest(".c-form__item").find(".c-label").append(required);
+	$(".form__field[required]").closest(".form__item").find(".label").append(required);
 
 	// check list
-	$(".c-check__input[required]").closest(".c-check").find(".c-check__label").append(required);
-	$(".c-form__list .c-check__input[required]").closest(".c-form__item").find(".c-label").append(required);
-	$(".c-form__list .c-check__label").find(".c-required").remove();
+	$(".check__input[required]").closest(".check").find(".check__label").append(required);
+	$(".form__list .check__input[required]").closest(".form__item").find(".label").append(required);
+	$(".form__list .check__label").find(".required").remove();
 }
