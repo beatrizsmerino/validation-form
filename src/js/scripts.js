d7a5bcaa9d0bf3bf8121197f3da1885c164d428d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-03-28 21:01:29 +0200

    Refactoring. Moved preventDefault() to main file 'scripts.js'

---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+/**
+ * @file scripts.js
+ * @description Main file
+ * @author Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
+ * @copyright (2021)
+ */
+
+
+
+
+
+(function () {
+	$(document).ready(function () {
+		$("form").submit(function (e) {
+			e.preventDefault();
+		});
+	});
+})();
