240012f6224523c6b86141397dec97432bdd505d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2026-03-23 21:04:55 +0100

    feat(html): make `.message` component announce content to screen readers
    Steps:
    - Move message text content from HTML to JS, leaving `.message__text` empty in the DOM
    - Add `role="alert"` and `aria-live="assertive"` to `.message__text` so screen readers detect content changes
    - Inject content dynamically via JS in `messageOpen()` and clear it in `messageClose()`
    - Move focus to `.message__text` with `tabindex="-1"` after opening so VoiceOver reads the content
    - Add 500ms delay before focus to allow VoiceOver to finish announcing the button first
    
    Reason:
    - VoiceOver only announces `aria-live` changes when content is dynamically added to the DOM, not when pre-existing content is shown/hidden via CSS
    
    References:
    - Issue #448

--- src/js/components/components-message.js
+++ src/js/components/components-message.js
@@ -2,11 +2,21 @@
 // MESSAGE
 // =================================================
 
+const messageContent =
+	'<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque perspiciatis veniam quidem eum enim! Repellendus beatae iste pariatur vero <a class="link" href="#" role="button" tabindex="0">necessitatibus consectetur</a> dolores dolorem distinctio iusto soluta, sapiente cumque eveniet porro!</p>';
+
 function messageOpen() {
 	$(".message__open").on("click", function(e) {
 		e.preventDefault();
-		$(".message").addClass("is-show");
-		$(".message").removeClass("is-hide");
+		const $message = $(".message");
+		const $text = $message.find(".message__text");
+		$message.addClass("is-show");
+		$message.removeClass("is-hide");
+		$text.html(messageContent);
+		$text.attr("tabindex", "-1");
+		setTimeout(function() {
+			$text.trigger("focus");
+		}, 3000);
 	});
 }
 
@@ -14,6 +24,7 @@ function messageClose() {
 	$(".message__close").on("click", function() {
 		$(".message").removeClass("is-show");
 		$(".message").addClass("is-hide");
+		$(".message__text").html("");
 	});
 }
 
